e3f35fa34fadc3f96b9049bcb6cca60a95483929
Josué Fonseca <47289940+josuefonseca@users.noreply.github.com>
2019-12-28 00:15:37 -0300

    v1.0
    Roda a cada 1 minuto no servidor da Heroku.
    
    Procura os tweets terminados em 'muse' a partir do último tweetado pelo bot,  cuja id está salva no log.

--- bot.js
+++ bot.js
@@ -31,15 +31,16 @@ function loop(){
 	
 	T.get('search/tweets', params, function(err, data, response) {
 		if(!err){  
-			
+			var last = -1;
 			for(var i = 0; i < data.statuses.length && tt == 0; i++){
 				
 				var text = data.statuses[i].text;				
 				
 				if(eValido(text)){
+					last++;
 					console.log('@' + data.statuses[i].user.screen_name + ': ' + text);
 					//tt++;
-					responder(data.statuses[i]);
+					responder(data.statuses[i], (last == 0));
 				}
 				
 			}
@@ -49,7 +50,7 @@ function loop(){
 	});
 }
 
-function responder(twt){
+function responder(twt, log){
 	//console.log('responder');
 	//console.log(twt);		
 	
@@ -66,14 +67,16 @@ function responder(twt){
 			console.log(err);
 			console.log();
 		} else {
-			var newLine = data.id_str + '\r\n';
-			fs.writeFile('./tweet_id.log', newLine,{enconding:'utf-8',flag: 'a'}, function (err) {
-				if (err) {
-					console.log('Erro ao salvar registro');
-				} else {
-					console.log(data.id_str + ' > tweet_id.log');
-				}
-			});
+			if(log){
+				var newLine = data.id_str + '\r\n';
+				fs.writeFile('./tweet_id.log', newLine,{enconding:'utf-8',flag: 'a'}, function (err) {
+					if (err) {
+						console.log('Erro ao salvar registro');
+					} else {
+						console.log(data.id_str + ' > tweet_id.log');
+					}
+				});
+			}
 			console.log('Tuitado com sucesso!');						
 		}	
 	}	
