819e1bfb8965176f52620e9601e721fba4c7ccab
Josué Fonseca <47289940+josuefonseca@users.noreply.github.com>
2019-12-28 00:07:28 -0300

    Initial commit

---
+++ bot.js
@@ -0,0 +1,94 @@
+console.log('eminem bot rodando');
+console.log();
+
+var Twit = require('twit');
+var config = require('./config');
+/*
+	console.log(config);
+	console.log();
+*/
+var T = new Twit(config);
+console.log(T);
+console.log();
+
+var fs = require('fs');
+var lastTweetId;
+
+var params = {
+	q: '"muse"',
+	lang: 'pt',
+	result_type: 'recent',
+	count: 100
+	since: lastTweetId;
+};
+var tt = 0;
+
+function loop(){ 
+	var log = fs.readFileSync('./tweet_id.log', 'utf8');
+	var linhas = log.split(/\r\n/);
+	lastTweetId = linhas[linhas.length-2];
+	console.log('Último tweet: ' + lastTweetId);
+	
+	T.get('search/tweets', params, function(err, data, response) {
+		if(!err){  
+			
+			for(var i = 0; i < data.statuses.length && tt == 0; i++){
+				
+				var text = data.statuses[i].text;				
+				
+				if(eValido(text)){
+					console.log('@' + data.statuses[i].user.screen_name + ': ' + text);
+					//tt++;
+					responder(data.statuses[i]);
+				}
+				
+			}
+			} else {
+			console.log(err);
+		}
+	});
+}
+
+function responder(twt){
+	//console.log('responder');
+	//console.log(twt);		
+	
+	var novaResposta = {
+		status: '@' + twt.user.screen_name + ' Eminem',
+		in_reply_to_status_id: twt.id_str
+	}
+	
+	T.post('statuses/update', novaResposta, tweeted);
+	
+	function tweeted(err, data, response){
+		if(err){
+			console.log('Algo deu errado.');
+			console.log(err);
+			console.log();
+		} else {
+			var newLine = data.id_str + '\r\n';
+			fs.writeFile('./tweet_id.log', newLine,{enconding:'utf-8',flag: 'a'}, function (err) {
+				if (err) {
+					console.log('Erro ao salvar registro');
+				} else {
+					console.log(data.id_str + ' > tweet_id.log');
+				}
+			});
+			console.log('Tuitado com sucesso!');						
+		}	
+	}	
+	
+}
+	
+	
+function eValido(txt){
+	str = new String(txt);
+	str = str.toLowerCase();
+
+	if(str.length = 4)
+	return str.endsWith('muse');
+	else
+	return str.endsWith(' muse') || str.endsWith(' @muse') || str.endsWith(' #muse');
+}
+
+setInterval(loop, 60000);
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key: 'qsMhVWG1kNq7Fge7hcQXSsvVX',
+	consumer_secret: 'i5AddBD1FtvbGhKkwxaMHtnrKKgs0Ern0vNyFmZ1YFpi6vtTvr',
+	access_token: '1209867533991460870-dTr3DvgVF71ufGObe4DaBdd6KXZmJ5',
+	access_token_secret: 'coMuwt8ZOYTVQ6K1hThnlIOKjpHPJudYUHcpPYxqYxgt0'
+}
